7b3af8b61448b0467826029bb24b1a2d50d2c481
Tim Vigers <timothy.vigers@ucdenver.edu>
2019-02-21 17:19:31 +0100

    Update app.R

--- CGM Analysis/shiny/cgmanalysis/app.R
+++ CGM Analysis/shiny/cgmanalysis/app.R
@@ -13,7 +13,6 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- 
-  
   fluidPage(
     titlePanel("Uploading Files"),
     sidebarLayout(
@@ -25,52 +24,20 @@ ui <-
                     'text/tab-separated-values',
                     'text/plain',
                     '.csv',
-                    '.tsv'
+                    '.txt'
                   )
-        ),
-        tags$hr(),
-        checkboxInput('header', 'Header', TRUE),
-        radioButtons('sep', 'Separator',
-                     c(Comma=',',
-                       Semicolon=';',
-                       Tab='\t'),
-                     ','),
-        radioButtons('quote', 'Quote',
-                     c(None='',
-                       'Double Quote'='"',
-                       'Single Quote'="'"),
-                     '"'),
-        tags$hr(),
-        p('If you want a sample .csv or .tsv file to upload,',
-          'you can first download the sample',
-          a(href = 'mtcars.csv', 'mtcars.csv'), 'or',
-          a(href = 'pressure.tsv', 'pressure.tsv'),
-          'files, and then try uploading them.'
-        )
+      )
       ),
-      mainPanel(
-        tableOutput('contents')
+      mainPanel(tableOutput('contents'))
       )
     )
-  )
-
-
-
 server <- function(input, output) {
   output$contents <- renderTable({
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
     inFile <- input$file1
-    
     if (is.null(inFile))
       return(NULL)
-    
-    read.csv(inFile$datapath, header = input$header,
-             sep = input$sep, quote = input$quote)
+    read.csv(inFile$datapath, header = T,
+             sep = ",")
   })
 }
 # Run the application 
